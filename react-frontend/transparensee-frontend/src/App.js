7fe75414410d683bbf787c0a5653c65302e38e5b
KiproticKibor <kiborotich24@gmail.com>
2024-10-17 03:41:17 +0300

    main file with routing

---
+++ react-frontend/transparensee-frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import SubmitReport from './pages/SubmitReport';
+import Investigation from './pages/Investigation';
+
+function App() {
+    return (
+        <AuthProvider>
+        <Router>
+            <div className="App">
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                    <Route path="/dashboard" component={Dashboard} />
+                    <Route path="/submit-report" component={SubmitReport} />
+                    <Route path="/investigation/:id" component={Investigation} />
+                </Switch>
+                <Footer />
+            </div>
+        </Router>
+        </AuthProvider>
+    );
+}
+
+export default App;

e29bb86f8c999912348eea67617314ab40de91e3
KiproticKibor <kiborotich24@gmail.com>
2024-10-17 13:17:08 +0300

    updated api.js file to include pagination and search functionality

--- react-frontend/transparensee-frontend/src/services/api.js
+++ react-frontend/transparensee-frontend/src/services/api.js
@@ -17,24 +17,42 @@ api.interceptors.request.use((config) => {
     return config;
 });
 
-export const getReports = async () => {
-    return await api.get('/reports');
+export const getReports = async (page = 1, search = '') => {
+    return await api.get('/reports/', {
+        params: {
+            page,
+            search,
+        },
+    });
 }
 export const getReport = async (id) => {
-    return await api.get(`/reports/${id}`);
+    return await api.get(`/reports/${id}/`);
 }
 export const createReport = async (data) => {
-    return await api.post('/reports', data);
+    return await api.post('/reports/', data);
 }
 
-export const getInvestigations = async () => {
-    return await api.get('/investigations');
+export const getInvestigations = async (page = 1, search = '') => {
+    return await api.get('/investigations/', {
+        params: {
+            page,
+            search,
+        },
+    });
 }
 export const getInvestigation = async (id) => {
-    return await api.get(`/investigations/${id}`);
+    return await api.get(`/investigations/${id}/`);
 }
 export const createContribution = async (investigationId, data) => {
-    return await api.post(`/investigations/${investigationId}/contributions`, data);
+    return await api.post(`/investigations/${investigationId}/contributions/`, data);
+}
+
+export const getUserProfile = async (username) => {
+    return await api.get('/users-profiles/${username}/');
+}
+
+export const updateProfile = async (username, data) => {
+    return await api.patch(`/users-profiles/${username}/`, data);
 }
 
 export default api;
